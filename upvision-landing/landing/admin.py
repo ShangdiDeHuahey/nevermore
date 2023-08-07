2b7c324dc9d6fbb53ae5368d05f8d86f422590ae
God's Scourge <134093220+ShangdiDeHuahey@users.noreply.github.com>
2023-08-07 19:50:41 +0600

    ds
    ds

--- upvision-landing/landing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ContactModel
-
-# Register your models here.
-
-class MyModelAdmin(admin.ModelAdmin):
-    readonly_fields = ('date_time',)
-
-admin.site.register(ContactModel, MyModelAdmin)
--- upvision/service/models.py
+++
@@ -1,5 +0,0 @@
-'''Данный модуль представляет из-за API для создания миграция в Базу Данных.'''
-
-from django.db import models
-
-# Create your models here.
